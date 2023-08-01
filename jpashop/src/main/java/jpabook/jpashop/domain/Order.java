80b0bdc1f8e602ec2ce4e5ea50ff252fb71bfe24
shihan00321 <shihan001@naver.com>
2023-08-01 16:51:01 +0900

    도메인 설계 모든 연관관계는 지연로딩으로 설정해야 한다. 즉시로딩은 예측이 어렵고, JPQL을 실행할 때 연관된 엔티티를 DB에서 함께 조회하기 위해 의도치 않은 쿼리가 발생하는 N + 1 문제가 자주 발생. 연관된 엔티티를 함께 조회해야 한다면 Fetch join 사용. @XToOne - Default(Eager)
    컬렉션은 필드에서 바로 초기화하는 것이 null 문제에 안전.
    
    연관관계 편의 메서드 - 양방향 연관관계에서 편의를 위해 양쪽 객체에 값을 설정해주는 편의 메서드 추가.

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -21,14 +21,14 @@ public class Order {
      * name -> 대상 엔티티와 매핑할 외래 키(자기 필드명) 이름을 지정해주는 값
      * referencedColumnName -> 조인할 컬럼명, default: 대상 테이블의 pk
      */
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", referencedColumnName = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id") //1:1관계에서는 더 많은 조회가 일어나는 쪽에 연관 관계 주인 설정.
     private Delivery delivery;
 
@@ -36,4 +36,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 메서드 ==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
