9fde6a51b8e6e9fba6ca6f4d88a2337407f51603
shihan00321 <shihan001@naver.com>
2023-08-01 18:13:59 +0900

    회원 레포지토리, 서비스, 테스트 개발
    MemberRepository - 기본적인 crud 개발
    MemberService - 중복 회원 검증 로직, 회원가입, 조회 기능 개발
    
    @RequiredArgsConstructor - final field에 대해서만 생성자를 만들어줌
    @AllArgsConstructor - 모든 field에 대해서 생성자를 만들어줌
    
    @Autowired 생성자가 하나일 때는 생략 가능
    생성자 주입 방식으로 불변하면서 안전한 객체 생성 가능
    
    MemberServiceTest
    @SpringBootTest - 스프링 부트를 띄우고 테스트함. (Autowired로 스프링 빈 주입)
    @Transactional - 테스트 케이스에서 사용 -> 테스트 수행 후 자동 롤백
    
    실제 서비스 환경과 테스트 환경은 다르므로 설정 파일을 다르게 생성해서 사용하는 것이 좋음
    별도의 설정이 없다면 자동으로 in-memory db가 사용된다.

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    public void 회원가입() {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertThat(member).isEqualTo(memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복회원예외() {
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        assertThatThrownBy(() -> memberService.join(member2)).isInstanceOf(IllegalStateException.class);
+    }
+}
