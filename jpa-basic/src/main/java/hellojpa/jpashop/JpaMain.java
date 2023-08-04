189f8faa0e78e42d32e314da13cb7674b081ea9e
shihan00321 <shihan001@naver.com>
2023-08-04 22:46:52 +0900

    영속성 컨텍스트란? 엔티티 매니저를 통해서 접근 가능한 논리적인 개념을 의미한다.
    비영속 : 영속성 컨텍스트와 전혀 관계가 없는 새로운 상태
    영속 : 영속성 컨텍스트에 관리되는 상태
    준영속 : 영속성 컨텍스트에 저장되었다가 분리된 상태
    삭제 : 영속성 컨텍스트에서 삭제된 상태
    
    영속성 컨텍스트의 이점
    1. 동일성(identity) 보장
    2. 1차 캐시 활용 가능
    3. 트랜잭션을 지원하는 쓰기 지연
    4. 변경 감지(Dirty chekcing)
    5. 지연 로딩
    
    플러시
    영속성 컨텍스트의 변경내용을 데이터베이스에 반영함.
    em.persist(E) -> 영속성 컨텍스트에 등록할 뿐 쿼리가 나가지 않음.
    실제로 쿼리는 플러시될 때 발생
    (영속성 컨텍스트를 비우는 것이 아니라 변경 내용을 동기화 하는 것)
    
    발생되는 경우
    1. 변경 감지가 일어나는 경우
    2. 쓰기 지연 SQL 저장소에 등록하는 경우
    
    플러시는 트랜잭션을 커밋하거나, JPQL 쿼리를 실행할 때 자동 호출 된다.
    
    @Entity
    기본 생성자 필수(파라미터가 없는 public 또는 protected 생성자)
    final, enum, interface, inner 클래스에는 사용X
    저장할 필드에 final 사용X
    
    hibernate.hbm2ddl.auto - option
    1. create : 기존테이블 삭제 후 다시 생성 (DROP + CREATE)
    2. create-drop : create + 종료 시점에 테이블 DROP
    3. update : 변경분만 반영(운영 DB에는 사용하면 안됨)
    4. validate : 엔티티와 테이블이 정상 매핑되었는지만 확인
    5. none : 사용X
    
    운영 장비에는 절대 create, create-drop, update 사용X.
    
    @GeneratedValue - option
    • IDENTITY: 데이터베이스에 위임, MYSQL
    • SEQUENCE: 데이터베이스 시퀀스 오브젝트 사용, ORACLE @SequenceGenerator 필요
    • TABLE: 키 생성용 테이블 사용, 모든 DB에서 사용 @TableGenerator 필요
    • AUTO: 방언에 따라 자동 지정, 기본값
    
    IDENTITY vs SEQUENCE 전략
    IDENTITY
    기본 키 생성을 데이터베이스에 위임 -> 영속성 컨텍스트에 등록되기 위해서는 PK가 있어야 함. -> 즉 데이터베이스에 INSERT SQL을 실행한 이후에 ID 값을 알 수 있음 -> em.persist 시점에 INSERT 쿼리를 실행. PK를 얻어온 뒤 영속성 컨텍스트에 등록 -> 쓰기 지연이 불가능하다.
    
    SEQUENCE
    유일한 값을 순서대로 생성하는 특별한 데이터베이스 오브젝트에서 PK 값을 얻어옴 ->
    즉 em.persist 시점에 INSERT 쿼리를 실행하지 않고도 영속성 컨텍스트에 등록이 가능함
    allocationSize - 시퀀스 한 번 호출에 증가하는 수
    
    데이터 중심 방식의 설계
    테이블의 외래키를 객체에 그대로 설계 -> 객체 간 참조가 불가능

---
+++ jpa-basic/src/main/java/hellojpa/jpashop/JpaMain.java
@@ -0,0 +1,52 @@
+package hellojpa.jpashop;
+
+import hellojpa.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("c");
+            System.out.println("============");
+            em.persist(member);
+            System.out.println("id : " + member.getId());
+            System.out.println("============");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void findMembers(EntityManager em) {
+//        List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                .setFirstResult(0 )
+//                .setMaxResults(5)
+//                .getResultList();
+//        for (Member member : result) {
+//            System.out.println("member = " + member.getName());
+//        }
+    }
+
+//    private static Member createMember() {
+//        //비영속 객체 생성
+//        Member member = new Member();
+//        member.setId(2L);
+//        member.setName("hello");
+//        // em.persist -> 영속 객체
+//        return member;
+//    }
+}
