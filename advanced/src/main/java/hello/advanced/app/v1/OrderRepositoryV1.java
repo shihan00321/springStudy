112da9531f3be4fab534c530682c5e5e873ff4fd
shihan00321 <shihan001@naver.com>
2024-02-26 02:42:36 +0900

    로그추적기 예제 개발

---
+++ advanced/src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.helloTrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+
+    private final HelloTraceV1 trace;
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int miilis) {
+        try {
+            Thread.sleep(miilis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ advanced/src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.helloTrace.HelloTraceV1;
+import hello.advanced.trace.helloTrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderServiceV2;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(@RequestParam("itemId") String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderServiceV2.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
