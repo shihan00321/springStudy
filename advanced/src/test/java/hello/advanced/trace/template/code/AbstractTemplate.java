a35a97051045b05f11e3094533154af8041ccc8e
shihan00321 <shihan001@naver.com>
2024-02-26 19:46:04 +0900

    기존 로그 추적기 - 코드의 중복이 많고, 핵심 기능이 아닌 부가 기능 코드가 많이 섞여 있음 - 변하는 것(핵심 로직)과 변하지 않는 것(로그)을 분리
    1. 템플릿 메서드 패턴 : 템플릿이라는 틀에 변하지 않는 부분을 몰아두고, 변하는 부분은 자식 클래스에 두어서 상속과 오버라이딩을 사용해서 처리. but 클래스를 계속 만들어야 하며, 자식 클래스에서 부모 클래스의 기능을 사용하지도 않는데 강하게 결합이 된다.

---
+++ advanced/src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,18 @@
+package hello.advanced.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        // log.info("비즈니스 로직1 실행"); // 변하는 부분
+        call();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    protected abstract void call();
+}
