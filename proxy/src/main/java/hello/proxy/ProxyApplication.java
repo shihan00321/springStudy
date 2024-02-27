04de27a21c06afc60a5344ab16d03a6026c2d586
shihan00321 <shihan001@naver.com>
2024-02-27 18:17:31 +0900

    스프링 부트 버전 변겅

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -3,7 +3,7 @@ package hello.proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
 	public static void main(String[] args) {
