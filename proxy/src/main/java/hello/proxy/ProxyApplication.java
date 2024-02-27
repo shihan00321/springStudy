0f3b6a6ad142bee2460a261065b95740d94ccc19
shihan00321 <shihan001@naver.com>
2024-02-27 19:10:37 +0900

    예제 프로젝트 init

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,13 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app")
+@Import({AppV1Config.class, AppV2Config.class}) // 아래에서 패키지를 제한했으므로 해당 config 수동 빈 등록
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") // 해당 패키지를 컴포넌트 스캔. default - 이 파일이 있는 패키지부터 하위 까지 스캔
 public class ProxyApplication {
 
 	public static void main(String[] args) {
