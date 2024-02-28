8bbe9b80c4e3f0c18a6a768c8132a3ad018467da
shihan00321 <shihan001@naver.com>
2024-02-28 20:57:43 +0900

    JDK 동적 프록시 - 프록시 클래스를 수없이 만들 필요없이 InvocationHandler를 만들어서 넣어주면 동적 프록시가 자동으로 생성됨, 인터페이스가 있을 때만 적용 가능

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,8 @@ import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +15,9 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class}) // 아래에서 패키지를 제한했으므로 해당 config 수동 빈 등록
 //@Import({InterfaceProxyConfig.class})
-@Import({ConcreteProxyConfig.class})
+//@Import({ConcreteProxyConfig.class})
+//@Import({DynamicProxyBasicConfig.class})
+@Import({DynamicProxyFilterConfig.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3") // 해당 패키지를 컴포넌트 스캔. default - 이 파일이 있는 패키지부터 하위 까지 스캔
 public class ProxyApplication {
 
