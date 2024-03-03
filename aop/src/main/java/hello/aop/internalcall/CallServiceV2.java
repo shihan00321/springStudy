7b3a032bf7b8d919aa05c4b474824141a60f8496
shihan00321 <shihan001@naver.com>
2024-03-04 01:44:38 +0900

    스프링 AOP 프록시와 내부 호출 문제 1. 자기 자신 의존 주입 방법 - 순환 참조 문제 발생 2. 스프링 빈 지연 조회 ObjectProvider(Provider), ApplicationContext를 이용해서 빈을 지연 조회한다. 3. 내부 호출이 발생하지 않도록 구조 변경! - 가장 이상적인 방법

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,30 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV2 {
+
+    // private final ApplicationContext applicationContext; 너무 많은 정보를 가져옴
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceProvider) {
+        this.callServiceProvider = callServiceProvider;
+    }
+
+    public void external() {
+        log.info("call external");
+        //CallServiceV2 callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        CallServiceV2 callServiceV2 = callServiceProvider.getObject();
+        callServiceV2.internal(); // 내부 메서드 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
---
+++ aop/src/test/java/hello/aop/internalcall/CallServiceV3Test.java
@@ -0,0 +1,23 @@
+package hello.aop.internalcall;
+
+import hello.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(CallLogAspect.class)
+@SpringBootTest
+@Slf4j
+class CallServiceV3Test {
+
+    @Autowired
+    CallServiceV3 callServiceV3;
+
+    @Test
+    void external() {
+        callServiceV3.external();
+    }
+
+}
