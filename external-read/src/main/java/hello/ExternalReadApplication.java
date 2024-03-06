738ca63f9d34019d56d3d4d93810dc96ee0166ec
shihan00321 <shihan001@naver.com>
2024-03-06 21:43:29 +0900

    외부 설정과 프로필 1. Environment 외부 설정들은 Environment를 통해서 일관되게 조회 가능하지만 값을 꺼내는 방식이 반복되고 Environment를 직접 주입받아야 함
    2. @Value - 값 주입
    외부 설정을 편하게 주입 받아올 수 있음
    - @Value("${외부 설정 키 값:defaultValue}") defaultValue 생략 가능
    하나하나 외부 설정의 값을 받아오는 것이 번거로움
    
    3. @ConfigurationProperties - 타입 안전한 설정 속성
    - 외부 설정의 묶음을 객체로 변환해서 가져옴.(@ConfigurationPropertie)
    - 생성자를 만들어 두면 생성자를 통해서 설정 정보를 주입 (생성자 바인딩 시 @ConstructorBinding 사용 - 생성자가 하나일 땐 생략 가능)
    @EnableConfigurationProperties - properties를 하나하나 빈 등록
    @ConfigurationPropertiesScan - 특정 범위를 묶음으로 빈 등록
    - 실수로 타입이 잘못 들어오는 경우도 방지 가능하고, 검증 가능
    -----
    @Profile("프로필 명") - 특정 조건에 따라서 해당 빈을 등록할지 말지 선택

---
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -0,0 +1,24 @@
+package hello;
+
+import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV2;
+import hello.config.MyDataSourceConfigV3;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Import;
+
+//@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceConfigV1.class)
+//@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
+@ConfigurationPropertiesScan({"hello.datasource"})
+public class ExternalReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalReadApplication.class, args);
+    }
+
+}
